37307c9b377f0887c4af101110f96105eaf88913
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-12 01:27:35 +0530

    my limited verilog knowledge!!!

--- section-2/led.v
+++ section-2/led.v
@@ -1,8 +1,9 @@
-module led_blink (
-input clk,
-output reg led
+module led (
+    input clk,
+    output reg led
 );
-
-// We want to blink an led here
-
+  // We want to blink an led here
+  always @(posedge clk) begin
+    led <= 1;
+  end
 endmodule
