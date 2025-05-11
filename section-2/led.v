180a442143a988b2b4c444e06a74645294be4ba4
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-12 01:44:16 +0530

    working blinker (i think)

--- section-2/led.v
+++ section-2/led.v
@@ -3,7 +3,13 @@ module led (
     output reg led
 );
   // We want to blink an led here
+  reg [31:0] count;
   always @(posedge clk) begin
-    led <= 1;
+    if (count == 999999999) begin  // Depends on the clock cycle:- 999999999 is 100 MHz
+      led   <= ~led;
+      count <= 0;
+    end else begin
+      count <= count + 1;
+    end
   end
 endmodule
