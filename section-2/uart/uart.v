10c700dd481ba814488ac54683749f9b2ad075a3
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-22 19:43:06 -0400

    some dummy code post big struggles
    INIT MAKING THIS WORK

--- section-2/uart/uart.v
+++ section-2/uart/uart.v
@@ -1,14 +1,29 @@
+`default_nettype none `timescale 1ns / 1ns
+
 module baud_gen #(
     parameter int BAUD_RATE = 115200,
     int CLK_FREQ = 100000000,  // 100 MHz
     int OVS_FACTOR = 16  // Oversampling Factor
 ) (
-    input  clk,
-    reset,
-    output tick_16x
+    input clk,
+    input reset,
+    output reg baud_tick,
+    output reg tick_16x
 );
-  localparam int DIVISORFP_16 = (CLK_FREQ << 16) / (BAUD_RATE * OVS_FACTOR);  // float divisor
+  localparam int unsigned DIVISORFP_16 = (CLK_FREQ << 24) / (BAUD_RATE * OVS_FACTOR);
+  localparam int unsigned DIVISORINT = DIVISORFP_16[31:24];
+  localparam int unsigned OVSWIDTH = $clog2(OVS_FACTOR);
+  reg [31:0] acc;
+  reg [OVSWIDTH-1:0] oversample_counter;
   always @(posedge clk) begin
+    if (reset) begin
+      acc <= 0;
+      oversample_counter <= 0;
+      baud_tick <= 1'b0;
+      tick_16x <= 1'b0;
+    end else begin
+      acc <= acc + 1;
+    end
   end
 endmodule
 
