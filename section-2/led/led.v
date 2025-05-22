2f5a86d2db4dfe49d47a93cf0e71908a0ce6a27a
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-22 17:58:47 -0400

    fix count logic

--- section-2/led/led.v
+++ section-2/led/led.v
@@ -5,7 +5,7 @@ module led (
   // We want to blink an led here
   reg [31:0] count;
   always @(posedge clk) begin
-    if (count == 999999999) begin  // Depends on the clock cycle:- 999999999 is 100 MHz
+    if (count == 99999999) begin  // Depends on the clock cycle:- 99999999 is 1 second on 100 MHz
       led   <= ~led;
       count <= 0;
     end else begin
