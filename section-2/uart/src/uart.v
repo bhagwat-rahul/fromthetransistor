fb81ea31ae4bff49425899e551c054bcb7022cf8
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-24 15:37:46 -0400

    instnaitate better, comment for now cz not everythings a wire

--- section-2/uart/src/uart.v
+++ section-2/uart/src/uart.v
@@ -5,15 +5,41 @@ module uart (
     reset
 );
 
-  wire baud_tick_1;
-  wire tick_16x_1;
+  // wire baud_tick, tick_16x;
+  // wire rx_pin, rx_data, data_ready, parity_err, frame_err;
+  // wire tx_data, send_request, tx_pin, tx_busy, tx_done;
+  // wire config_bits;
 
-  baud_gen baud_gen_a (
-      .clk(clk),
-      .reset(reset),
-      .baud_tick(baud_tick_1),
-      .tick_16x(tick_16x_1)
-  );
+  // baud_gen baud_gen_a (
+  //     .clk,
+  //     .reset,
+  //     .baud_tick,
+  //     .tick_16x
+  // );
+
+  // uart_rx uart_rx_a (
+  //     .clk,
+  //     .reset,
+  //     .tick_16x,
+  //     .rx_pin,
+  //     .config_bits,
+  //     .rx_data,
+  //     .data_ready,
+  //     .parity_err,
+  //     .frame_err
+  // );
+
+  // uart_tx uart_tx_a (
+  //     .clk,
+  //     .reset,
+  //     .baud_tick,
+  //     .tx_data,
+  //     .send_request,
+  //     .config_bits,
+  //     .tx_pin,
+  //     .tx_busy,
+  //     .tx_done
+  // );
 
   always @(posedge clk) begin
     if (reset) begin
