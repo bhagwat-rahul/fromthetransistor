b7df5ca2d5748d4008d97985e197848273691706
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-24 17:51:16 -0400

    init assembler

--- section-3/assembler.py
+++ section-3/assembler.py
@@ -5,13 +5,30 @@ def main():
     instruction = input()
     encode_instruction(instruction) # Add rs1 and rs2 and store result in rd
 
+# R (register-register ALU instructions), I (ALU immediate instructions, load instructions), S (store instructions, comparison and branch instructions), B (branch instructions), U (jump instructions, jump and link instructions), and J (jump instructions)
+
+R_instr = []
+I_instr = []
+S_instr = []
+B_instr = []
+U_instr = []
+J_instr = []
+
 def encode_instruction(instruction: str):
-  if instruction.__contains__("add "):
-    print("add")
-  elif instruction.__contains__("sub "):
-    print("subtract")
+  if instruction in (R_instr):
+    print("register")
+  elif instruction in (I_instr):
+    print("immediate")
+  elif instruction in (S_instr):
+    print("store")
+  elif instruction in (B_instr):
+    print("branch")
+  elif instruction in (U_instr):
+    print("jump-link")
+  elif instruction in (B_instr):
+    print("jump")
   else:
-    print("that doesn't look like valid risc-v assembly")
+    print("invalid risc-v assembly")
 
 if __name__ == "__main__":
     main()
