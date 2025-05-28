8b7db0c3abe26e84bccedc7d05b056eca5e83891
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-28 12:39:34 -0400

    fix linter errors and finish up top module

--- section-2/uart/src/uart.v
+++ section-2/uart/src/uart.v
@@ -1,49 +1,52 @@
 `default_nettype none `timescale 1ns / 1ns
 
 module uart (
-    input logic clk,
-    input logic reset
+    input logic       clk,
+    input logic       reset,
+    input logic       rx_pin,
+    input logic       send_request,
+    input logic [7:0] tx_data,
+    input logic       parity_enable,
+
+    output logic [7:0] rx_data,
+    output logic       data_ready,
+    output logic       parity_err,
+    output logic       frame_err,
+    output logic       tx_pin,
+    output logic       tx_busy,
+    output logic       tx_done
 );
 
-  // wire baud_tick, tick_16x;
-  // wire rx_pin, rx_data, data_ready, parity_err, frame_err;
-  // wire tx_data, send_request, tx_pin, tx_busy, tx_done;
-  // wire config_bits;
+  logic baud_tick, tick_16x;
 
-  // baud_gen baud_gen_a (
-  //     .clk,
-  //     .reset,
-  //     .baud_tick,
-  //     .tick_16x
-  // );
+  baud_gen baud_gen_a (
+      .clk(clk),
+      .reset(reset),
+      .baud_tick(baud_tick),
+      .tick_16x(tick_16x)
+  );
 
-  // uart_rx uart_rx_a (
-  //     .clk,
-  //     .reset,
-  //     .tick_16x,
-  //     .rx_pin,
-  //     .parity_enable,
-  //     .rx_data,
-  //     .data_ready,
-  //     .parity_err,
-  //     .frame_err
-  // );
+  uart_rx uart_rx_a (
+      .clk(clk),
+      .reset(reset),
+      .tick_16x(tick_16x),
+      .rx_pin(rx_pin),
+      .parity_enable(parity_enable),
+      .rx_data(rx_data),
+      .data_ready(data_ready),
+      .parity_err(parity_err),
+      .frame_err(frame_err)
+  );
 
-  // uart_tx uart_tx_a (
-  //     .clk,
-  //     .reset,
-  //     .baud_tick,
-  //     .tx_data,
-  //     .send_request,
-  //     .parity_enable,
-  //     .tx_pin,
-  //     .tx_busy,
-  //     .tx_done
-  // );
-
-  always @(posedge clk) begin
-    if (reset) begin
-
-    end
-  end
+  uart_tx uart_tx_a (
+      .clk(clk),
+      .reset(reset),
+      .baud_tick(baud_tick),
+      .tx_data(tx_data),
+      .send_request(send_request),
+      .parity_enable(parity_enable),
+      .tx_pin(tx_pin),
+      .tx_busy(tx_busy),
+      .tx_done(tx_done)
+  );
 endmodule
