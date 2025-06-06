a3cc47d140af6ee3e3463151e000df755c9362c5
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-06 16:09:50 -0400

    much cleaner state machine ... i think?

--- section-2/uart/src/uart_tx.v
+++ section-2/uart/src/uart_tx.v
@@ -24,76 +24,90 @@ module uart_tx #(
   } fsm_e;
 
   localparam int unsigned INDEXWIDTH = $clog2(DATA_BITS);
-  logic [INDEXWIDTH-1:0] bit_index;
-  logic [ DATA_BITS-1:0] tx_shift;
+  logic [INDEXWIDTH-1:0] bit_index, next_bit_index;
+  logic [DATA_BITS-1:0] tx_shift, next_tx_shift;
+  logic tx_pin_reg, next_tx_pin;
+  logic tx_busy_reg, next_tx_busy;
+  logic tx_done_reg, next_tx_done;
   fsm_e tx_state, next_tx_state;
 
   always @(posedge clk or posedge reset) begin
     if (reset) begin
-      tx_pin <= 1'b1;
-      tx_shift <= {DATA_BITS{1'b1}};
-      tx_state <= IDLE;
-      bit_index <= 0;
-      tx_busy <= 0;
-      tx_done <= 0;
+      tx_shift    <= {DATA_BITS{1'b1}};
+      tx_state    <= IDLE;
+      bit_index   <= 0;
+      tx_pin_reg  <= 1;
+      tx_busy_reg <= 0;
+      tx_done_reg <= 0;
     end else begin
-      case (tx_state)
-        default: begin
-          tx_state <= IDLE;
-        end
-        IDLE: begin
-          tx_done <= 0;
-          tx_busy <= 1'b0;
-          tx_pin  <= 1'b1;
-          if (send_request == 1) begin
-            tx_shift <= tx_data;
-            tx_busy  <= 1'b1;
-            tx_state <= START;
-          end
-        end
-        START, DATA, ODD_PARITY, STOP, DONE: begin
-          if (baud_tick) begin
-            case (tx_state)
-              default: begin
-                tx_state <= IDLE;
-              end
-              START: begin
-                tx_pin <= 1'b0;
-                tx_busy <= 1;
-                bit_index <= 0;
-                tx_state <= DATA;
-              end
-              DATA: begin
-                tx_busy <= 1;
-                tx_pin  <= tx_shift[bit_index];
-                if (bit_index == INDEXWIDTH'(DATA_BITS - 1)) begin
-                  if (parity_enable) begin
-                    tx_state <= ODD_PARITY;
-                  end else begin
-                    tx_state <= STOP;
-                  end
-                end else begin
-                  bit_index <= bit_index + 1;
-                end
-              end
-              ODD_PARITY: begin
-                tx_pin   <= ~^tx_shift;  // To complete odd parity
-                tx_state <= STOP;
-              end
-              STOP: begin
-                tx_busy  <= 1;
-                tx_pin   <= 1'b1;
-                tx_state <= DONE;
-              end
-              DONE: begin
-                tx_done  <= 1;
-                tx_busy  <= 0;
-                tx_state <= IDLE;
-              end
-            endcase
-          end
-        end
-      endcase
+      if (baud_tick) begin
+        tx_state    <= next_tx_state;
+        bit_index   <= next_bit_index;
+        tx_shift    <= next_tx_shift;
+        tx_pin_reg  <= next_tx_pin;
+        tx_busy_reg <= next_tx_busy;
+        tx_done_reg <= next_tx_done;
+      end
     end
   end
+
+  always_comb begin
+    next_tx_state  = tx_state;
+    next_bit_index = bit_index;
+    next_tx_shift  = tx_shift;
+    next_tx_pin    = tx_pin_reg;
+    next_tx_busy   = tx_busy_reg;
+    next_tx_done   = tx_done_reg;
+    case (tx_state)
+      default: next_tx_state = IDLE;
+      IDLE: begin
+        next_tx_done = 0;
+        next_tx_busy = 0;
+        next_tx_pin  = 1;
+        if (send_request == 1) begin
+          next_tx_shift = tx_data;
+          next_tx_busy  = 1;
+          next_tx_state = START;
+        end
+      end
+      START: begin
+        next_tx_state  = DATA;
+        next_tx_pin    = 0;
+        next_tx_busy   = 1;
+        next_bit_index = 0;
+      end
+      DATA: begin
+        next_tx_busy = 1;
+        next_tx_pin  = tx_shift[bit_index];
+        if (bit_index == (DATA_BITS - 1)) begin
+          next_tx_state  = fsm_e'(parity_enable) ? ODD_PARITY : STOP;
+          next_bit_index = 0;
+        end else begin
+          next_tx_state  = DATA;
+          next_bit_index = bit_index + 1;
+        end
+      end
+      ODD_PARITY: begin
+        next_tx_pin   = ~^tx_shift;
+        next_tx_busy  = 1;
+        next_tx_state = STOP;
+      end
+      STOP: begin
+        next_tx_busy  = 1;
+        next_tx_pin   = 1;
+        next_tx_state = DONE;
+      end
+      DONE: begin
+        next_tx_done  = 1;
+        next_tx_busy  = 0;
+        next_tx_state = IDLE;
+        next_tx_pin   = 1;
+      end
+    endcase
+  end
+
+  assign tx_pin  = tx_pin_reg;
+  assign tx_busy = tx_busy_reg;
+  assign tx_done = tx_done_reg;
+
 endmodule
