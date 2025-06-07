f86165def14e9d08e01d7e73d65082bd7dc14edd
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-06 20:38:09 -0400

    noob ish compiler start

--- section-4/compiler/compiler.hs
+++ section-4/compiler/compiler.hs
@@ -1,2 +1,12 @@
 -- This is a 64 bit risc-v c compiler
 main = putStrLn "Hello, World! I am a riscv-64 compiler!"
+
+keywords = ["return", "if", "while", "for", "switch", "typedef", "sizeof", "else", "do", "break", "continue"]
+
+datatypes = ["int", "void", "char", "bool", "float", "double", "short", "long", "struct", "union", "enum"]
+
+operators = ["+", "-", "*", "/", "%", "&", "|", "||", "&&", "=", "==", "!=", "<", ">", "<=", ">=", "!", "~", "^"]
+
+punctuation = ["{", "}", "(", ")", ",", ";", ":"]
+
+directives = ["#include", "#define", "#ifdef", "#ifndef", "#endif", "#undef", "#pragma"]
