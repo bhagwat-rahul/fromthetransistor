ff13e1e8e1ac6b62401ecfd5a196a7a5b7200619
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-09 14:25:12 -0400

    some bug bashing

--- section-2/uart/src/uart_rx.v
+++ section-2/uart/src/uart_rx.v
@@ -35,6 +35,9 @@ module uart_rx #(
   logic [BITINDEXWIDTH-1:0] bit_index, next_bit_index;
   logic midsample = (os_count == OVSWIDTH'(MIDSAMPLE));
   logic lasttick = (os_count == OVSWIDTH'(LASTTICK));
+  logic lastbit = (bit_index == DATA_BITS);
+  logic [DATA_BITS-1:0] next_rx_data;
+  logic next_data_ready, next_parity_err, next_frame_err;
 
   always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
@@ -46,12 +49,26 @@ module uart_rx #(
       os_count   <= 0;
       rx_shift   <= 0;
       bit_index  <= 0;
-    end else begin
-      rx_state <= next_rx_state;
+    end else if (tick_16x) begin
+      if (midsample) begin
+        // Midsample stuff
+        os_count <= os_count + 1;
+      end else if (lasttick) begin
+        // Last tick stuff
+        os_count <= 0;
+      end else begin
+        // Do when neither
+        os_count <= os_count + 1;
+      end
     end
   end
 
   always_comb begin
+    next_frame_err  = 0;
+    next_rx_data    = 0;
+    next_parity_err = 0;
+    next_data_ready = 0;
+    next_bit_index  = 0;
     case (rx_state)
       default: next_rx_state = IDLE;
       IDLE: begin
@@ -61,24 +78,24 @@ module uart_rx #(
 
       end
       DATA: begin
-        if (bit_index == DATA_BITS) begin
+        if (lastbit) begin
           next_rx_state  = (parity_enable) ? ODD_PARITY : STOP;
           next_bit_index = 0;
         end else begin
-          next_rx_state      = DATA;
-          rx_data[bit_index] = rx_pin;
-          next_bit_index     = bit_index + 1;
+          next_rx_state           = DATA;
+          next_rx_data[bit_index] = rx_pin;
+          next_bit_index          = bit_index + 1;
         end
       end
       ODD_PARITY: begin
       end
       STOP: begin
-        frame_err     = (rx_pin == 0) ? 1 : 0;
-        next_rx_state = DONE;
+        next_frame_err = (rx_pin == 0) ? 1 : 0;
+        next_rx_state  = DONE;
       end
       DONE: begin
-        data_ready    = 1;
-        next_rx_state = IDLE;
+        next_data_ready = 1;
+        next_rx_state   = IDLE;
       end
     endcase
   end
