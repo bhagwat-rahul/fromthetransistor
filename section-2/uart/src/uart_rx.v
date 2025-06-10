c09409dc8ea595799c1c79b8c442d4a52e9cade7
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-10 12:24:57 -0400

    almost working (kinda oversamples?)

--- section-2/uart/src/uart_rx.v
+++ section-2/uart/src/uart_rx.v
@@ -33,7 +33,7 @@ module uart_rx #(
   logic [OVSWIDTH-1:0] os_count, next_os_count;
   logic [DATA_BITS-1:0] rx_shift;
   logic [BITINDEXWIDTH-1:0] bit_index, next_bit_index;
-  logic midsample;
+  logic midsample, start, next_start;
   logic lasttick;
   logic lastbit;
   logic next_data_ready, next_parity_err, next_frame_err;
@@ -55,6 +55,7 @@ module uart_rx #(
       rx_data_reg    <= 0;
       parity_err_reg <= 0;
       data_ready_reg <= 0;
+      start          <= 0;
     end else if (tick_16x) begin
       rx_data_reg    <= next_rx_data_reg;
       rx_state       <= next_rx_state;
@@ -63,40 +64,42 @@ module uart_rx #(
       data_ready_reg <= next_data_ready;
       parity_err_reg <= next_parity_err;
       frame_err_reg  <= next_frame_err;
+      start          <= next_start;
     end
   end
 
   always_comb begin
-    lasttick = (os_count == OVSWIDTH'(LASTTICK));
-    midsample = (os_count == OVSWIDTH'(MIDSAMPLE));
-    lastbit = (bit_index == BITINDEXWIDTH'(DATA_BITS - 1));
+    lasttick         = (os_count == OVSWIDTH'(LASTTICK));
+    midsample        = (os_count == OVSWIDTH'(MIDSAMPLE));
+    lastbit          = (bit_index == BITINDEXWIDTH'(DATA_BITS - 1));
     next_rx_state    = rx_state;
     next_os_count    = os_count;
     next_bit_index   = bit_index;
-    next_data_ready  = 0;
+    next_data_ready  = data_ready;
     next_parity_err  = parity_err_reg;
     next_frame_err   = frame_err_reg;
     next_rx_data_reg = rx_data_reg;
+    next_start       = start;
     if (rx_state == IDLE) begin
-      next_rx_state   = fsm_e'((rx_pin == 0) ? START : IDLE);
-      next_os_count   = 0;
-      next_parity_err = 0;
-      next_frame_err  = 0;
+      if (rx_pin == 0) begin
+        next_rx_state    = START;
+        next_os_count    = 0;
+        next_data_ready  = 0;
+        next_rx_data_reg = 0;
+      end
     end else if (lasttick) begin
       next_os_count = 0;
       case (rx_state)
-        default: next_rx_state = IDLE;
+        default: next_rx_state = rx_state;
         START: begin
-          next_rx_state = DATA;
+          next_rx_state = fsm_e'((start) ? DATA : IDLE);
         end
         DATA: begin
           if (lastbit) begin
-            next_rx_state = fsm_e'((parity_enable) ? ODD_PARITY : STOP);
-            next_rx_data_reg[bit_index] = rx_pin;
+            next_rx_state  = fsm_e'((parity_enable) ? ODD_PARITY : STOP);
             next_bit_index = 0;
           end else begin
             next_bit_index = bit_index + 1;
-            next_rx_data_reg[bit_index] = rx_pin;
           end
         end
         ODD_PARITY: begin
@@ -104,13 +107,28 @@ module uart_rx #(
           next_rx_state   = STOP;
         end
         STOP: begin
-          next_frame_err = (rx_pin == 0) ? 1 : 0;
-          next_rx_state  = DONE;
+          next_frame_err  = (rx_pin == 0) ? 1 : 0;
+          next_rx_state   = DONE;
+          next_data_ready = 1;
         end
         DONE: begin
-          next_data_ready = 1;
-          next_rx_state   = IDLE;
+          next_frame_err   = 0;
+          next_parity_err  = 0;
+          next_data_ready  = 0;
+          next_rx_state    = IDLE;
+          next_rx_data_reg = 0;
         end
+      endcase
+    end else if (midsample) begin
+      next_os_count = os_count + 1;
+      unique case (rx_state)
+        IDLE: ;
+        START: next_start = (rx_pin == 0);
+        DATA: next_rx_data_reg[bit_index] = rx_pin;
+        ODD_PARITY: ;
+        STOP: ;
+        DONE: ;
+        default: $display("should never be default");
       endcase
     end else begin
       next_os_count = os_count + 1;
