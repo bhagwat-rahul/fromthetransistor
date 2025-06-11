837a1698b44008e0dd625aaeca328d4b4afca1a6
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-10 20:11:37 -0400

    some init stuff (kinda getting ahead of myself)

--- section-3/riscv/src/riscv.v
+++ section-3/riscv/src/riscv.v
@@ -4,4 +4,17 @@ module riscv (
     input clk,
     input reset
 );
+
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      // Do reset things (init counters and load bootrom?)
+    end else begin
+
+    end
+  end
+
+  always_comb begin
+
+  end
+
 endmodule
