2c325186243ce8db9f08a3976cc5d879b99d532b
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-12 10:49:33 -0400

    some trash i cooked up

--- section-3/riscv/src/riscv.v
+++ section-3/riscv/src/riscv.v
@@ -5,9 +5,17 @@ module riscv (
     input reset
 );
 
+  localparam int unsigned ADDRWIDTH = 8;
+  localparam int unsigned DATAWIDTH = 64;
+
+  logic [ADDRWIDTH-1:0] bromaddr;
+  logic [DATAWIDTH-1:0] bromrdata;
+
   always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-      // Do reset things (init counters and load bootrom?)
+      // TODO: Random inits, fix
+      bromaddr  <= 0;
+      bromrdata <= 0;
     end else begin
 
     end
@@ -16,5 +24,15 @@ module riscv (
   always_comb begin
 
   end
+
+  bootrom #(
+      .INIT_FILE ("bootrom.bin"),  // gen from ../bootrom.asm
+      .ADDR_WIDTH(ADDRWIDTH),
+      .DATA_WIDTH(DATAWIDTH)
+  ) bootrom (
+      .clk,
+      .addr (bromaddr),
+      .rdata(bromrdata)
+  );
 
 endmodule
