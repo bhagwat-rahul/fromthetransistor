ca9874eae55db477939f7f3f253025966f5b0b24
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-12 15:19:01 -0400

    begin pipelining

---
+++ section-3/riscv/src/pipeline/writeback.v
@@ -0,0 +1,23 @@
+`default_nettype none `timescale 1ns / 1ns
+
+// Write back
+
+module writeback #(
+
+) (
+    input logic clk,
+    input logic resetn
+);
+  always_ff @(posedge clk or negedge resetn) begin
+    if (resetn == 0) begin
+
+    end else begin
+
+    end
+  end
+
+  always_comb begin
+
+  end
+
+endmodule
--- section-3/riscv/src/riscv.v
+++ section-3/riscv/src/riscv.v
@@ -1,38 +1,24 @@
 `default_nettype none `timescale 1ns / 1ns
 
-module riscv (
+module riscv #(
+    parameter logic [63:0] RESETVEC = 64'hx8000_0000
+) (
     input clk,
-    input reset
+    input resetn
 );
 
-  localparam int unsigned ADDRWIDTH = 8;
-  localparam int unsigned DATAWIDTH = 64;
+  reg [63:0] pc, next_pc;
 
-  logic [ADDRWIDTH-1:0] bromaddr;
-  logic [DATAWIDTH-1:0] bromrdata;
-
-  always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-      // TODO: Random inits, fix
-      bromaddr  <= 0;
-      bromrdata <= 0;
+  always_ff @(posedge clk or negedge resetn) begin
+    if (!resetn) begin
+      pc <= RESETVEC;
     end else begin
-
+      pc <= next_pc;
     end
   end
 
   always_comb begin
-
+    next_pc = pc;  // stay the same if nothing
   end
-
-  bootrom #(
-      .INIT_FILE ("bootrom.bin"),  // gen from ../bootrom.asm
-      .ADDR_WIDTH(ADDRWIDTH),
-      .DATA_WIDTH(DATAWIDTH)
-  ) bootrom (
-      .clk,
-      .addr (bromaddr),
-      .rdata(bromrdata)
-  );
 
 endmodule
