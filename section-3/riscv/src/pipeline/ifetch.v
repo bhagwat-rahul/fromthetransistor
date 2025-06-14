8dfc3b698d1d056414797a5d3409252b0557f54f
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-13 20:43:51 -0400

    more pipeline init

--- section-3/riscv/src/pipeline/ifetch.v
+++ section-3/riscv/src/pipeline/ifetch.v
@@ -5,9 +5,24 @@
 module ifetch #(
 
 ) (
-    input logic clk,
-    input logic resetn
+    input logic        clk,
+    input logic        resetn,
+    input logic        pc_valid,
+    input logic        stall,
+    input logic        flush,
+    input logic        imem_valid,
+    input logic        imem_ready,
+    input logic        imem_err,
+    input logic [63:0] pc_next,
+    input logic [63:0] imem_data,
+
+    output logic        imem_req,
+    output logic        instr_valid,
+    output logic [31:0] instruction,
+    output logic [63:0] pc_current,
+    output logic [31:0] imem_addr
 );
+
   always_ff @(posedge clk or negedge resetn) begin
     if (resetn == 0) begin
 
