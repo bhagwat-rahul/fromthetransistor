a7d989a1c63c3ebd319fe6f9c5b24b808dc4dec1
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-23 20:51:03 -0400

    init ins

--- section-3/riscv/src/pipeline/exec.v
+++ section-3/riscv/src/pipeline/exec.v
@@ -3,10 +3,20 @@
 // Execute / ALU
 
 module exec #(
-
+    parameter logic [8:0] XLEN = 9'd64
 ) (
     input logic clk,
-    input logic resetn
+    input logic resetn,
+    input logic [XLEN-1:0] pc,
+    input logic [31:0] instr,
+    input logic [5:0] opcode,
+    input logic [1:0] funct_3,
+    input logic [5:0] funct_7,
+    input logic [XLEN-1:0] imm,
+    input logic [3:0] rs1_addr,
+    input logic [3:0] rs2_addr,
+    input logic [3:0] rd_addr
+
 );
   always_ff @(posedge clk or negedge resetn) begin
     if (resetn == 0) begin
