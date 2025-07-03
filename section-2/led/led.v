407d82bc67fb1b7665fe235750e55d3b75b1e74f
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-03 14:23:04 -0400

    buncha makefile and yosys changes

--- section-2/led/led.v
+++ section-2/led/led.v
@@ -14,7 +14,6 @@ module led (
     end else if (count == 99999999) begin
       // Depends on the clock cycle:- 99999999 is 1 second on 100 MHz
       led_1 <= ~led_1;
-      $display("LED toggled at time %0t ns", $time);
       count <= 0;
     end else begin
       count <= count + 1;
