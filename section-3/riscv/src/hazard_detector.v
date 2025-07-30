196de18bc97459cac4047f0f96ac944a73c6a7e1
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-30 16:57:39 -0400

    getting there...

---
+++ section-3/riscv/src/hazard_detector.v
@@ -0,0 +1,10 @@
+`default_nettype none `timescale 1ns / 1ns
+
+module hazard_detector #(
+    parameter logic [8:0] XLEN = 9'd64
+) (
+    input clk,
+    input resetn
+);
+
+endmodule
---
+++ section-3/riscv/src/memory_controller.v
@@ -0,0 +1,10 @@
+`default_nettype none `timescale 1ns / 1ns
+
+module memory_controller #(
+    parameter logic [8:0] XLEN = 9'd64
+) (
+    input clk,
+    input resetn
+);
+
+endmodule
