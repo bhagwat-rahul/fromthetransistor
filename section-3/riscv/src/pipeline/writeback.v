74cd1242876d39288cd310aa013417457b5f57eb
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-30 14:47:22 -0400

    writeback done (kinda)

--- section-3/riscv/src/pipeline/writeback.v
+++ section-3/riscv/src/pipeline/writeback.v
@@ -5,19 +5,41 @@
 module writeback #(
     parameter logic [8:0] XLEN = 9'd64
 ) (
-    input logic clk,
-    input logic resetn
+    input logic             clk,
+    input logic             resetn,
+    input logic             stall,
+    input logic             flush,
+    input logic [     4:0]  rd_in,
+    input logic             reg_write_enable_in,
+    input logic [XLEN-1:0]  writeback_data_in,
+    input logic             exception_occurred_in,
+    input logic [XLEN-1:0]  exception_pc_in,
+    input logic [     3:0]  exception_cause_in,
+
+    output logic [     4:0] regfile_rd,
+    output logic [XLEN-1:0] regfile_wd,
+    output logic            regfile_we,
+    output logic            exception_out,
+    output logic [XLEN-1:0] exception_pc_out,
+    output logic [     3:0] exception_cause_out
 );
-  always_ff @(posedge clk or negedge resetn) begin
-    if (resetn == 0) begin
 
+always_comb begin
+    if (!resetn) begin
+        regfile_rd          = 5'd0;
+        regfile_wd          = '0;
+        regfile_we          = 1'b0;
+        exception_out       = 1'b0;
+        exception_pc_out    = '0;
+        exception_cause_out = 4'd0;
     end else begin
-
+        regfile_rd          = rd_in;
+        regfile_wd          = writeback_data_in;
+        regfile_we          = reg_write_enable_in && !exception_occurred_in && !flush && !stall;
+        exception_out       = exception_occurred_in && !flush;
+        exception_pc_out    = exception_pc_in;
+        exception_cause_out = exception_cause_in;
     end
-  end
-
-  always_comb begin
-
-  end
+end
 
 endmodule
--- section-3/riscv/test/writeback_tb.v
+++ section-3/riscv/test/writeback_tb.v
@@ -5,11 +5,54 @@ module writeback_tb #(
 ) ();
 
   logic clk, resetn;
+  logic             stall;
+  logic             flush;
+  logic [     4:0]  rd_in;
+  logic             reg_write_enable_in;
+  logic [XLEN-1:0]  writeback_data_in;
+  logic             exception_occurred_in;
+  logic [XLEN-1:0]  exception_pc_in;
+  logic [     3:0]  exception_cause_in;
+
+  logic [     4:0] regfile_rd;
+  logic [XLEN-1:0] regfile_wd;
+  logic            regfile_we;
+  logic            exception_out;
+  logic [XLEN-1:0] exception_pc_out;
+  logic [     3:0] exception_cause_out;
 
   writeback #(
       .XLEN(XLEN)
   ) writeback_a (
       .clk,
-      .resetn
+      .resetn,
+      .stall,
+      .flush,
+      .rd_in,
+      .reg_write_enable_in,
+      .writeback_data_in,
+      .exception_occurred_in,
+      .exception_pc_in,
+      .exception_cause_in,
+      .regfile_rd,
+      .regfile_wd,
+      .regfile_we,
+      .exception_out,
+      .exception_pc_out,
+      .exception_cause_out
   );
+
+  /* verilator lint_off BLKSEQ */
+  always #5 clk = ~clk;
+  /* verilator lint_on BLKSEQ */
+
+  initial begin
+    clk    = 0;
+    resetn = 0;
+    #50
+    resetn = 1;
+    $display("reset asserted");
+    $finish;
+  end
+
 endmodule
